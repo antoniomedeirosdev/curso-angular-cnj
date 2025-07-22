45bee3afcccf28a69effff01f20d3e713e3c5699
Antônio Vinícius Menezes Medeiros <contato@antoniomedeiros.dev>
2025-07-22 18:43:29 -0300

    Polimorfismo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import Professor from "./person/Professor";
+import Circle from "./interface/Circle";
+import Shape from "./interface/Shape";
+import Triangle from "./interface/Triangle";
 
-const bruno = new Professor("Bruno", "Lopes", ["Curso Angular", "Programação Funcional"]);
-console.log(bruno.fullName());
-console.log(bruno.subjects);
+const tr = new Triangle(3, 8);
+const cr = new Circle(3);
+
+function imprimirArea(shape: Shape) {
+  console.log(shape.calculateArea());
+}
+
+imprimirArea(tr);
+imprimirArea(cr);
