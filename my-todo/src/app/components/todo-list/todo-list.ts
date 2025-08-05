9158142907f4bd632d8edbfc701c346d9cb26151
Antônio Vinícius Menezes Medeiros <contato@antoniomedeiros.dev>
2025-08-05 19:59:15 -0300

    HttpClient - Curso Angular - Aula 06 - Parte 03

--- my-todo/src/app/components/todo-list/todo-list.ts
+++ my-todo/src/app/components/todo-list/todo-list.ts
@@ -1,8 +1,11 @@
 import { NgIf, NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { v4 as uuidv4 } from 'uuid';
 
 interface Task {
+  id: string;
   name: string;
 }
 
@@ -14,13 +17,20 @@ interface Task {
   styleUrl: './todo-list.css',
 })
 export class TodoListComponent {
+
+  constructor(private http:HttpClient){}
+
   tasks: Task[] = [];
 
   @ViewChild('todoName') todoInputRef: ElementRef<HTMLInputElement> = null!;
 
+  ngOnInit() {
+    this.loadTasks();
+  }
+
   addTask(name: string) {
     if (name) {
-      this.tasks.push({ name: name });
+      this.tasks.push({ id: uuidv4(), name: name });
       console.log(name);
       this.todoInputRef.nativeElement.value = '';
     }
@@ -28,5 +38,16 @@ export class TodoListComponent {
 
   removeTask() {
     this.tasks.pop();
+  }
+
+  loadTasks() {
+    this.http.get<Task[]>('http://localhost:3000/tasks')
+      .toPromise()
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 }
