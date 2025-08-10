141a30c92dcae812e6663577b45d71e10d76c9e2
Antônio Vinícius Menezes Medeiros <contato@antoniomedeiros.dev>
2025-08-10 01:37:04 -0300

    NgForm - Curso Angular - Aula 09 - Parte 01

--- simple-blog-base/src/app/components/dashboard-post-form-dialog/dashboard-post-form-dialog.ts
+++ simple-blog-base/src/app/components/dashboard-post-form-dialog/dashboard-post-form-dialog.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { FormsModule, NgForm } from '@angular/forms';
+
 import { MatDialogModule } from '@angular/material/dialog'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
@@ -7,10 +9,19 @@ import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-dashboard-post-form-dialog',
-  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [FormsModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './dashboard-post-form-dialog.html',
   styleUrl: './dashboard-post-form-dialog.css'
 })
 export class DashboardPostFormDialog {
+
+  onSubmit(postForm: NgForm) {
+    const author = postForm.form.value.author;
+    const title = postForm.form.value.title;
+    const date = postForm.form.value.date;
+    const content = postForm.form.value.content;
+
+    console.log(`${author} ${title} ${date} ${content}`)
+  }
 
 }
